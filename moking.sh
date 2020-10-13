3cb5c4297bb57de45325c5669aebe3e72b2888b9
Moking001 <68329910+Moking001@users.noreply.github.com>
2020-10-13 13:50:10 +0430

    Add files via upload

---
+++ moking.sh
@@ -0,0 +1,46 @@
+
+
+
+#Mod by        : Moking
+#Tool Unfaedah : h4ker;"m"
+
+clear
+
+                                          
+
+ echo $mer"               WhatsappHack by moking | telegram chanel t.me/h4kermoking              "
+ echo ""
+ echo -n $cy" Enter Whatsapp number you want hack: "
+ ##### LOGO #####
+logo = """
+              Mőķìñğ h4ķèr                  
+_______________███__██
+______________███___ ███
+_____________███_____███_██
+____________███______███_███
+___________███______███__███
+_________████_____████__███
+_________████___████___███_██
+_________██████████___███_███
+________███████████_████_███
+________███████████████_███
+________█████████████_█████
+████_███████████████_████
+█████_██████████████_████
+██████_████████████_████
+███████_██████████████
+████████_███████████
+█████████_███████
+██████████
+██████████
+ read t
+ echo $pur"<•======♤=====h4ket=====♤=====•>"
+ echo -n $cy" Click on verification on ok Whatsapp and then tipe ok: "
+ read t
+ echo $pur"<•=♡====♤====Moking=====♤===♡==•>"
+ echo $me "Verification Code loading "
+ sleep 15
+ 
+ #!/bin/bash
+
+ echo Code:$((RANDOM + 456907))
